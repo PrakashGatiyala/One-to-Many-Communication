bf1a7f64dbc2c865021891247d3ffb48db7c6ef3
prakashgatiyala <ipg_2016069@iiitm.ac.in>
2020-08-19 17:09:33 +0530

    first commit

---
+++ api/config.php
@@ -0,0 +1,24 @@
+<?php
+///////////////////////////////////////////////////////
+//
+// Application: Sample App - 1 to 1 Video Call using EnableX Web Toolkit 1.3
+// Version: 1.0.0
+//
+// This Sample Application demonstrates the use of Server APIs and Web Toolkit of EnableX platform to build 
+// an one to one Video Conference
+//
+// Released: Apr 18, 2019
+//
+// File: config.php
+// Configuration File - needs to be updated to meet your Application requirment
+//
+//
+/////////////////////////////////////////////////////
+
+define("API_URL",	"https://api.enablex.io/v1");
+define("APP_ID",	"5f31485e90ef80d018498f94");
+define("APP_KEY",	"u4uAyaaPaEaauEe7e8aMeHuMaGuruba5yPyG");
+
+// NOTE! Your Application Server must be hosted on HTTPS. 
+
+?>
